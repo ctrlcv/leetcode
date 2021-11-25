d41a12e46fc36212a787b1c6e06c8e475303e083
Hyungoo Kim <ctrlcvis@gmail.com>
2021-11-25 15:21:05 +0900

    20211125

--- src/com/foriton/leetcode/Number0008StringToInteger.java
+++ src/com/foriton/leetcode/Number0008StringToInteger.java
@@ -1,11 +1,21 @@
 package com.foriton.leetcode;
 
+import java.nio.charset.StandardCharsets;
+
 public class Number0008StringToInteger {
     public static void run() {
         System.out.println(myAtoi("042"));
     }
 
     public static int myAtoi(String s) {
-        return 42;
+        int result = 0;
+
+        byte[] bytes = s.getBytes(StandardCharsets.US_ASCII);
+
+        for (int i = 0; i < bytes.length; i++) {
+            //
+        }
+
+        return result;
     }
 }
